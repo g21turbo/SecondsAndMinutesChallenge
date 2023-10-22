6e75e9956031c4d824a148ac99c5ea10364463ae
g21turbo <dg21stew@gmail.com>
2023-10-22 10:47:58 -0700

    Corrected input validation and removed duplicate code by calling the overloaded method in the original method

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,48 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(getDurationString(265, 32));
+        System.out.println(getDurationString(3945));
 
-        System.out.println(getDurationString(5873));
+        System.out.println(getDurationString(-3945));
+
+        System.out.println(getDurationString(65, 59));
+
+        System.out.println(getDurationString(-65, -45));
+
+        System.out.println(getDurationString(65, -45));
+
+
 
     }
 
     public static String getDurationString(int seconds) {
 
         if (seconds < 0) {
-            return "Invalid Value";
-        } else {
-            int hours = seconds/3600;
-            int minutes = hours%60;
-            int remainingSeconds = minutes%60 + seconds%60;
-
-            return hours + "h " + minutes + "m " + remainingSeconds + "s";
+            return "Invalid data for seconds(" + seconds
+                    + "), must be a positive integer value";
         }
 
+        return getDurationString(seconds / 60, seconds % 60);
     }
+
 
     public static String getDurationString(int minutes, int seconds) {
 
-        if (minutes < 0 || seconds > 59 || seconds < 0) {
-            return "Invalid Values";
-        } else {
-            int hours = minutes / 60;
-            int remainingMinutes = minutes%60;
-            return hours + "h " + remainingMinutes + "m " + seconds + "s";
+        if (minutes < 0) {
+            return "Invalid data for minutes(" + minutes
+                    + "), must be a positive integer value";
         }
+
+        if (seconds <= 0 || seconds >= 60) {
+            return "Invalid data for seconds(" + seconds
+                    + "), must be between 0 and 60";
+        }
+
+        int hours = minutes / 60;
+
+        int remainingMinutes = minutes % 60;
+
+        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+
     }
 }
